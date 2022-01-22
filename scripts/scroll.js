04970e1c21b70d64d5c12e4769edbeb6e77b3ca7
nenormalnui <72319525+nenormalnui@users.noreply.github.com>
2022-01-23 01:41:51 +0300

    Add files via upload

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,36 +1,42 @@
-const links = document.querySelectorAll('.header-menu__item a')
-const allcharachteristics = document.querySelector('.card-details__link-characteristics')
+const ScrollFunc = () => {
+  const links = document.querySelectorAll(".header-menu__item a");
+  const allcharachteristics = document.querySelector(".card-details__link-characteristics");
 
-seamless.polyfill();
+  seamless.polyfill();
 
-links.forEach((element) => {
-    element.addEventListener('click', (event) => {
-        event.preventDefault();
+  links.forEach((element) => {
+    element.addEventListener("click", (event) => {
+      event.preventDefault();
 
-        const id=element.getAttribute('href').substring(1)
-        const section=document.getElementById(id)
-        if(section){
-            seamless.elementScrollIntoView(section, {
-                behavior:"smooth",
-                block:"start"})
-        } else {
-            seamless.elementScrollIntoView(document.querySelector("#target"), {
-                behavior: "smooth",
-                block: "center",
-                inline: "center",
-            });
-        }
-    })
-})
+      const id = element.getAttribute("href").substring(1);
+      const section = document.getElementById(id);
+      if (section) {
+        seamless.elementScrollIntoView(section, {
+          behavior: "smooth",
+          block: "start",
+        });
+      } else {
+        seamless.elementScrollIntoView(document.querySelector("#target"), {
+          behavior: "smooth",
+          block: "center",
+          inline: "center",
+        });
+      }
+    });
+  });
 
-allcharachteristics.addEventListener('click', (event) => {
+  allcharachteristics.addEventListener("click", (event) => {
     event.preventDefault();
 
-    const allcharachteristicsid=allcharachteristics.getAttribute('href').substring(1)
-    const allcharachteristicssection=document.getElementById(allcharachteristicsid)
+    const allcharachteristicsid = allcharachteristics.getAttribute("href").substring(1);
+    const allcharachteristicssection = document.getElementById(allcharachteristicsid);
+
     seamless.elementScrollIntoView(allcharachteristicssection, {
-        behavior: "smooth",
-        block: "center",
-        inline: "center"
-    })
-})
+      behavior: "smooth",
+      block: "center",
+      inline: "center",
+    });
+  });
+};
+
+ScrollFunc();
